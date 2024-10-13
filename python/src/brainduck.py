904b4f5ae778960356b022827d682c67a978fc58
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-10-13 02:51:00 -0300

    feat(python): improve arguments parsing

--- python/src/brainduck.py
+++ python/src/brainduck.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     # Testing if some option isn't valid
     if invalid_option := args_parser.has_invalid_option(VALID_OPTIONS):
         print(f"Error: Invalid option '{invalid_option}'")
-        exit(0)
+        exit(1)
 
     # Setting the configs based on the selected options
     debug_mode = args_parser.option_pos("debug") != -1
@@ -43,10 +43,14 @@ if __name__ == '__main__':
     # Getting the path, if it has one
     if len(args_parser.argv) > 1:
         code_path = args_parser.argv[-1]
+        if not code_path.endswith(".bf"):
+            print("Error: Only Brainfuck files (.bf) can be used.")
+            exit(1)
+    
     # If it doesn't, but is defined to compile the code, it will not run
-    elif compile_mode is True:
+    if compile_mode is True and len(args_parser.argv) < 2:
         print("Error: The code can only be compiled if it has an source file.")
-        exit(0)
+        exit(1)
 
     # Running the main function
     run(code_path, debug_mode, compile_mode)
