baed0eef8508eebf48cc7a19bae040d87e52a824
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-10-13 02:09:01 -0300

    feat(python): improve compilation
    better, lighter, faster

--- python/modules/compiler.py
+++ python/modules/compiler.py
@@ -7,32 +7,31 @@ class BrainduckCompiler:
     def __init__(self):
         self.c_code: list[str] = [
             "#include <stdio.h>",
-            "#include <stdlib.h>",
-            "#include <stdint.h>",
             "int main() { ",
-            "uint8_t *p = (uint8_t *) calloc(30000, 1);",
-            "if (p == NULL) { return 1; }"
+            "unsigned char d[65535];",
+            "unsigned short p = 0;",
+            "if (d == NULL) { return 1; }"
         ]
 
     def run(self, bf_code: list[str], debug_mode=False) -> int:
         for cmd in bf_code:
             match cmd:
                 case "+":
-                    self.c_code.append("++*p;")
+                    self.c_code.append("d[p]++;")
                 case "-":
-                    self.c_code.append("--*p;")
+                    self.c_code.append("d[p]--;")
                 case ">":
-                    self.c_code.append("++p;")
+                    self.c_code.append("p++;")
                 case "<":
-                    self.c_code.append("--p;")
+                    self.c_code.append("p--;")
                 case "[":
-                    self.c_code.append("while (*p != 0) {")
+                    self.c_code.append("while (d[p] != 0) {")
                 case "]":
                     self.c_code.append("}")
                 case ".":
-                    self.c_code.append("putchar(*p);")
+                    self.c_code.append("putchar(d[p]);")
                 case ",":
-                    self.c_code.append("*p = getchar();")
+                    self.c_code.append("d[p] = getchar();")
 
         #
         self.c_code.append("return 0;\n}")
